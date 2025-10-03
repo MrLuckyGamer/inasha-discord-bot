6aab3465fdb4136006dbca6284f40548798e91b1
Lucky <43052474+MrLuckyGamer@users.noreply.github.com>
2025-10-03 20:15:07 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const { Client, GatewayIntentBits, Collection, EmbedBuilder } = require("discord.js");
 const fs = require("fs");
 const path = require("path");
 const config = require("./config.json");
