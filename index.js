770e118ee92240ee70851f4ca746b6500a13abf5
Lucky <43052474+MrLuckyGamer@users.noreply.github.com>
2025-10-16 19:07:48 +1100

    ephemeral deprecated error fix
    ephemeral: true has been deprecated
    Using flags: MessageFlags.Ephemeral instead

--- index.js
+++ index.js
@@ -212,7 +212,10 @@ client.on("interactionCreate", async (interaction) => {
     await command.execute(interaction, client);
   } catch (error) {
     console.error(error);
-    await interaction.reply({ content: "There was an error executing this command.", ephemeral: true });
+    await interaction.reply({
+      content: "There was an error executing this command.",
+      flags: require("discord.js").MessageFlags.Ephemeral,
+    });
   }
 });
 
