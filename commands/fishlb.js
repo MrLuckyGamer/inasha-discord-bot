776fc202a7e2ff4b554363c87ad531fa571494a2
Lucky <43052474+MrLuckyGamer@users.noreply.github.com>
2025-10-20 14:02:41 +1100

    Update fishlb.js

--- commands/fishlb.js
+++ commands/fishlb.js
@@ -55,7 +55,7 @@ module.exports = {
 
     const nextButton = new ButtonBuilder()
       .setCustomId("next")
-      .setLabel("⏭️ Next")
+      .setLabel("Next ⏭️")
       .setStyle(ButtonStyle.Secondary)
       .setDisabled(totalPages <= 1);
 
