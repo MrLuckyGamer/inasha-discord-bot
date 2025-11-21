c3aaf50b36242c2050d8203d852e8f0fc904c2a6
Lucky <43052474+MrLuckyGamer@users.noreply.github.com>
2025-11-21 20:20:20 +1100

    more fixes

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder } = require("discord.js");
+const { EmbedBuilder, ChannelType } = require("discord.js");
 const config = require("../config.json");
 const { version: botVersion } = require("../package.json");
 
@@ -9,6 +9,19 @@ module.exports = {
   async execute(message) {
     const client = message.client;
 
+    const totalUsers = client.guilds.cache.reduce((sum, g) => sum + g.memberCount, 0);
+
+    let totalChannels = 0;
+    client.guilds.cache.forEach(guild => {
+      totalChannels += guild.channels.cache.filter(c => 
+        c.type === ChannelType.GuildText || c.type === ChannelType.GuildVoice
+      ).size;
+    });
+
+    const uptimeDays = Math.floor(client.uptime / 86400000);
+    const uptimeHours = Math.floor((client.uptime % 86400000) / 3600000);
+    const uptimeMinutes = Math.floor((client.uptime % 3600000) / 60000);
+
     const createdDate = client.user.createdAt.toLocaleDateString("en-GB");
 
     const embed = new EmbedBuilder()
@@ -16,22 +29,18 @@ module.exports = {
       .setThumbnail(client.user.displayAvatarURL())
       .addFields(
         { name: "Servers", value: `${client.guilds.cache.size}`, inline: true },
-        { name: "Users", value: `${client.users.cache.size}`, inline: true },
-        { name: "Channels", value: `${client.channels.cache.size}`, inline: true },
+        { name: "Users", value: `${totalUsers}`, inline: true },
+        { name: "Channels", value: `${totalChannels}`, inline: true },
         { name: "Prefix", value: `\`${config.prefix}\``, inline: true },
         { name: "Version", value: botVersion, inline: true },
-
         { name: "Node.js", value: process.version, inline: true },
         { name: "Discord.js", value: `v${require("discord.js").version}`, inline: true },
-        //{ name: "Lodash", value: `v${require("lodash/package.json").version}`, inline: true },
-        //{ name: "Undici", value: `v${require("undici/package.json").version}`, inline: true },
-
         { name: "Created On", value: createdDate, inline: true },
         { name: "Owner", value: `<@320407113887252482>`, inline: true }
       )
       .setColor(8388736)
       .setTimestamp();
 
-    message.channel.send({ embeds: [embed] });
+    return message.channel.send({ embeds: [embed] });
   },
 };
--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,4 +1,5 @@
-const { EmbedBuilder } = require("discord.js");
+const { EmbedBuilder, ChannelType } = require("discord.js");
+const { getBotCount } = require("./serverstats");
 
 module.exports = {
   name: "serverinfo",
@@ -6,14 +7,22 @@ module.exports = {
   category: "Utility",
   async execute(message) {
     const guild = message.guild;
+    if (!guild) return message.reply("This command can only be used in a server.");
+
+    const bots = await getBotCount(guild).catch(() => 0);
+    const users = Math.max(0, guild.memberCount - bots);
+
+    const textChannels = guild.channels.cache.filter(c => c.type === ChannelType.GuildText).size;
+    const voiceChannels = guild.channels.cache.filter(c => c.type === ChannelType.GuildVoice).size;
 
     const embed = new EmbedBuilder()
       .setTitle(`Server Info: ${guild.name}`)
       .setThumbnail(guild.iconURL({ dynamic: true }))
       .addFields(
         { name: "Owner", value: `<@${guild.ownerId}>`, inline: true },
-        { name: "Members", value: `${guild.memberCount}`, inline: true },
-        { name: "Channels", value: `${guild.channels.cache.size}`, inline: true },
+        { name: "Users", value: `${users}`, inline: true },
+        { name: "Text Channels", value: `${textChannels}`, inline: true },
+        { name: "Voice Channels", value: `${voiceChannels}`, inline: true },
         { name: "Roles", value: `${guild.roles.cache.size}`, inline: true },
         { name: "Boosts", value: `${guild.premiumSubscriptionCount}`, inline: true },
         { name: "Created On", value: `<t:${Math.floor(guild.createdTimestamp / 1000)}:D>`, inline: true }
@@ -21,6 +30,6 @@ module.exports = {
       .setColor(8388736)
       .setTimestamp();
 
-    message.channel.send({ embeds: [embed] });
+    return message.channel.send({ embeds: [embed] });
   },
 };
