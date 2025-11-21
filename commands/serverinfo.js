80278ff7d872d1f7c8c275a7e0b61743d7d632d9
Lucky <43052474+MrLuckyGamer@users.noreply.github.com>
2025-11-21 20:30:07 +1100

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,5 +1,5 @@
 const { EmbedBuilder, ChannelType } = require("discord.js");
-const { getBotCount } = require("./serverstats");
+const { getBotCount } = require("./commands/serverstats.js");
 
 module.exports = {
   name: "serverinfo",
@@ -12,17 +12,17 @@ module.exports = {
     const bots = await getBotCount(guild).catch(() => 0);
     const users = Math.max(0, guild.memberCount - bots);
 
-    const textChannels = guild.channels.cache.filter(c => c.type === ChannelType.GuildText).size;
-    const voiceChannels = guild.channels.cache.filter(c => c.type === ChannelType.GuildVoice).size;
+    const totalChannels = guild.channels.cache.filter(ch => 
+      ch.type === ChannelType.GuildText || ch.type === ChannelType.GuildVoice
+    ).size;
 
     const embed = new EmbedBuilder()
       .setTitle(`Server Info: ${guild.name}`)
       .setThumbnail(guild.iconURL({ dynamic: true }))
       .addFields(
         { name: "Owner", value: `<@${guild.ownerId}>`, inline: true },
-        { name: "Users", value: `${users}`, inline: true },
-        { name: "Text Channels", value: `${textChannels}`, inline: true },
-        { name: "Voice Channels", value: `${voiceChannels}`, inline: true },
+        { name: "Members", value: `${users}`, inline: true },
+        { name: "Channels", value: `${totalChannels}`, inline: true },
         { name: "Roles", value: `${guild.roles.cache.size}`, inline: true },
         { name: "Boosts", value: `${guild.premiumSubscriptionCount}`, inline: true },
         { name: "Created On", value: `<t:${Math.floor(guild.createdTimestamp / 1000)}:D>`, inline: true }
