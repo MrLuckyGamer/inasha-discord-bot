bdbff230bd1c4a8f98386c3e70c6447e9e39a664
Lucky <43052474+MrLuckyGamer@users.noreply.github.com>
2026-01-11 01:17:38 +1100

    Revert "Reapply "Add comprehensive music playback commands and queue system""
    This reverts commit 6904e0aca81429ae5a76d5beb5399a243af968bd.

--- index.js
+++ index.js
@@ -26,27 +26,12 @@ client.slashCommands = new Collection();
 
 // === Load Prefix Commands ===
 const commandsPath = path.join(__dirname, "commands");
-
-function loadCommands(dir) {
-  const files = fs.readdirSync(dir);
-  
-  for (const file of files) {
-    const filePath = path.join(dir, file);
-    const stat = fs.statSync(filePath);
-    
-    if (stat.isDirectory()) {
-      loadCommands(filePath);
-    } else if (file.endsWith('.js')) {
-      const command = require(filePath);
-      if (command?.name) {
-        client.commands.set(command.name, command);
-      }
-    }
-  }
-}
-
 if (fs.existsSync(commandsPath)) {
-  loadCommands(commandsPath);
+  const commandFiles = fs.readdirSync(commandsPath).filter(f => f.endsWith(".js"));
+  for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    if (command?.name) client.commands.set(command.name, command);
+  }
 }
 
 // === Load Slash Commands ===
